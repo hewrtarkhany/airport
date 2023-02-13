6ffec4871e725cc9afa74457309039bfa63ccf70
hewrtarkhany <hewrtarkhany@gmail.com>
2023-02-12 22:28:23 -0700

    fixing AddressRepo

--- src/main/java/airport/backend/repository/AddressRepository.java
+++ src/main/java/airport/backend/repository/AddressRepository.java
@@ -1,8 +1,9 @@
 package airport.backend.repository;
 
+import airport.backend.model.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class AddressRepository {
-
+public interface AddressRepository extends JpaRepository<Address, Integer> {
 }
