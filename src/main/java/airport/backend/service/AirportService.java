0e42f4f605f26c0a06e0f2177a80a291df91d39a
hewrtarkhany <hewrtarkhany@gmail.com>
2023-02-12 22:10:51 -0700

    modifying service

--- src/main/java/airport/backend/service/AirportService.java
+++ src/main/java/airport/backend/service/AirportService.java
@@ -1,10 +1,16 @@
 package airport.backend.service;
 
+import airport.backend.model.Address;
 import airport.backend.model.Airport;
+import airport.backend.model.Person;
 
 import java.util.List;
 
 public interface AirportService  {
     public Airport saveAirport(Airport airport);
     public List <Airport> getAllAirports();
+    public Person savePerson(Person person);
+    public List<Person> getAllPerson();
+    public Address saveAddress(Address address);
+    public List<Address>getAllAddress();
 }
